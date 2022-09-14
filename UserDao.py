7cbef3830e8864ac742517a1ef10d8b1801fcec5
Damian Lobos <damianlobos24@gmail.com>
2022-09-14 09:43:35 -0300

    Insert method created

--- UserDao.py
+++ UserDao.py
@@ -5,6 +5,7 @@ class UserDao:
     _SELECT = "SELECT * FROM users"
     _DELETE = "DELETE FROM users WHERE id_user = %s"
     _UPDATE = "UPDATE users set user_name = %s, password = %s WHERE id_user = %s"
+    _INSERT = "INSERT INTO users (id_user, user_name, password) VALUES (%s, %s, %s)"
 
     @classmethod
     def selectAll(cls):
@@ -29,4 +30,13 @@ class UserDao:
             userData = (user.userName, user.password, user.userId)
             cursor.execute(cls._UPDATE, userData)
 
+    @classmethod
+    def insertUser(cls, user):
+        with Cursor() as cursor:
+            userData = (user.userId, user.userName, user.password)
+            cursor.execute(cls._INSERT, userData)
+
+user = User(3, "Chatrancito", "24101994zoe")
+UserDao.insertUser(user)
+UserDao.selectAll()
 
